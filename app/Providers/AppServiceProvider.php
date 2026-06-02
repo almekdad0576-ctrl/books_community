f955452e0475ed9183dcaa14f9db32bf4acac69f
Almekdad <almekdad.0576@gmail.com>
2026-06-03 00:37:32 +0300

    fix import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Services\AuthService;
 use App\Services\BookService;
 use App\Services\CommentService;
